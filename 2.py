39d7fe87e638a6f5d3874fe2b5cd49b944a04462
Odyssey Meltonyan <odissey.meltonyan@gmail.com>
2023-11-12 14:44:37 +0300

    Commit 2.py

---
+++ 2.py
@@ -0,0 +1,13 @@
+def main():
+    try:
+        user_input1 = int(input("Enter first number: "))
+        user_input2 = int(input("Enter second number: "))
+        user_input3 = int(input("Enter third number: "))
+    
+        if (user_input1 % 2 == 0) or (user_input2 % 2 == 0) or (user_input3 % 2 == 0): return True
+        else: return False
+    except:
+        return "Enter a number please"
+
+if __name__ == "__main__":
+    print(main())
