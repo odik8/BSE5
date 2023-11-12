3039524a0380cb987395edebb51319e7bf739e40
Odyssey Meltonyan <odissey.meltonyan@gmail.com>
2023-11-12 14:43:59 +0300

    Commit 1.py

---
+++ 1.py
@@ -0,0 +1,16 @@
+def main():
+    try:
+        n = int(input("Enter kW: "))
+        payment = 0
+        for i in range(1, n+1):
+            if i <= 250: payment += 7
+            elif 250 < i <= 300: payment += 17
+            else: payment += 20 
+
+        return payment
+    except:
+        return "Enter a positive integer"
+
+
+if __name__ == "__main__":
+    print(main())
